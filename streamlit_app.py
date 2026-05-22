7766b970ad21b9ff804ee1d74c510396a30f1f36
naelyluthfiyah-commits <naelyluthfiyah@gmail.com>
2026-05-22 14:33:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,16 +4,5 @@ st.title("🎈 Project Kelompok 11")
 import datetime
 import streamlit as st
 
-today = datetime.datetime.now()
-next_year = today.year + 1
-jan_1 = datetime.date(next_year, 1, 1)
-dec_31 = datetime.date(next_year, 12, 31)
-
-d = st.date_input(
-    "Select your vacation for next year",
-    (jan_1, datetime.date(next_year, 1, 7)),
-    jan_1,
-    dec_31,
-    format="MM.DD.YYYY",
-)
-d
+d = st.date_input("When's your birthday", datetime.date(2019, 7, 6))
+st.write("Your birthday is:", d)
