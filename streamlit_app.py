e6ceafa4e15d1d0be9cc3b0c162e88eed8443417
naelyluthfiyah-commits <naelyluthfiyah@gmail.com>
2026-05-23 09:19:41 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,3 @@
 import streamlit as st
 
 st.title("🎈 Project Kelompok 11")
-import datetime
-import streamlit as st
-
-d = st.date_input("When's your birthday", datetime.date(2006, 12, 22))
-st.write("Your birthday is:", d)
